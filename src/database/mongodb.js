d1a594d2a02be08a60d4e41dbf6bb91486d99e79
airtonlima <jose.airtonlimajr@gmail.com>
2021-08-03 22:23:36 -0300

    mongo config

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const config = {
-    IP: process.env.MONGODB_IP,
+    HOST: process.env.MONGODB_HOST,
     PORT: process.env.MONGODB_PORT,
     USERNAME: process.env.MONGODB_USERNAME,
     PASSWORD: process.env.MONGODB_PASSWORD,
@@ -17,7 +17,8 @@ class MongoDB
     {
         // Replace the following with your MongoDB deployment's connection string.
         // ${config.USERNAME}:${config.PASSWORD}@
-        const URI = `mongodb://${config.IP}:${config.PORT}/?autdhSource=admin&readPreference=primary&ssl=false`;
+        // const URI = `mongodb://${config.IP}:${config.PORT}/?autdhSource=admin&readPreference=primary&ssl=false`;
+        const URI = `mongodb+srv://${config.USERNAME}:${config.PASSWORD}@${config.HOST}?retryWrites=true&w=majority`;
         return await new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
     }
 
@@ -92,7 +93,7 @@ class MongoDB
             const database = client.db(config.DATABASE);
             const collection = database.collection(config.COLLECTION);
 
-            return await collection.insert(params);
+            return await collection.insertOne(params);
             
         } catch (err) {
             throw err;
